144777b4d531ee755719f3522a67e9464d0b1636
mixa <mihsa11ru@gmail.com>
2021-12-11 16:10:47 +0300

    Initial Commit

---
+++ sort/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  sort
+//
+//  Created by Михаил Козлов on 11.12.2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
